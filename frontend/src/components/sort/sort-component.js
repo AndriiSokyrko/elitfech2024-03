3724c5ca14e936d063f1f3cac8ec36c523618a24
Sokyrko Andrii <sokyrko.andrii@gmail.com>
2024-03-06 20:56:37 +0200

    Base level add sort design v1

--- frontend/src/components/sort/sort-component.js
+++ frontend/src/components/sort/sort-component.js
@@ -4,7 +4,7 @@ function SortComponent({sortItems}) {
     return (
         <select onChange={e=>sortItems(e.target.value)} className="clx_sort" defaultValue="DEFAULT">
             <option value="DEFAULT" disabled >Choose ...</option>
-            <option value="price"   selected="selected">price</option>
+            <option value="price">price</option>
             <option value="data">data</option>
         </select>
     );
